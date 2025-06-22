b1481654b933befdfb45bfbd335ec1d1be1e1d8d
Cassie Andrews <candrews1@ufl.edu>
2025-06-22 15:44:55 -0400

    update state.js

--- src/context/book/state.js
+++ src/context/book/state.js
@@ -3,9 +3,13 @@ let favoriteBooks = []
 
 try {
     // try to get items from localStorage
+    const storedFavorites = localStorage.getItem('favoriteBooks')
     // if items are found, display them for favoriteBooks
-} catch {
+    favoriteBooks = storedFavorites ? JSON.parse(storedFavorites) : []
+} catch (error) {
     // catch error and display [] for favoriteBooks
+    console.error('Error')
+    favoriteBooks = []
 }
 
 // initial state should have keys bookSearchResults and favoriteBooks
@@ -13,7 +17,7 @@ const initialState = {
     // bookSearchResults should be an empty array
     bookSearchResults: [],
     // favoriteBooks should be the value from localStorage or an empty array if localStorage value is falsy
-    favoriteBooks: []
+    favoriteBooks: favoriteBooks
 }
 
 // Export initial state object as default
